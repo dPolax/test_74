b9002bb409f8363103a738bf4098efd3ad06010e
dPolax <ragnaris2011@gmail.com>
2025-03-17 21:29:51 +0200

    3

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app=Flask(__name__)
 
-
+@app.route('/index')
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'about!'
 
 if __name__ == '__main__':
     app.run(debug=True)
